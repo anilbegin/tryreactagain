d7d6b6848078b0f29c99825b847d48f79fa5dbf0
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-03-09 22:18:00 +0530

    completed 58 60 61 from csstrans to search part1

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,9 +1,16 @@
 import React, { useContext, useEffect } from "react"
-
+import { useImmer } from "use-immer"
+import Axios from "axios"
 import DispatchContext from "../DispatchContext"
 
 function Search() {
   const appDispatch = useContext(DispatchContext)
+  const [state, setState] = useImmer({
+    searchTerm: "",
+    results: [],
+    show: "neither",
+    requestCount: 0
+  })
 
   useEffect(() => {
     document.addEventListener("keyup", searchKeyPressHandler)
@@ -16,6 +23,54 @@ function Search() {
     }
   }
 
+  useEffect(() => {
+    if (state.searchTerm.trim()) {
+      setState(draft => {
+        draft.show = "loading"
+      })
+      const delay = setTimeout(() => {
+        setState(draft => {
+          draft.requestCount++
+        })
+      }, 3000)
+
+      return () => clearTimeout(delay)
+    } else {
+      setState(draft => {
+        draft.show = "neither"
+      })
+    }
+  }, [state.searchTerm])
+
+  useEffect(() => {
+    if (state.requestCount) {
+      const ourRequest = Axios.CancelToken.source()
+      async function fetchResults() {
+        try {
+          const response = await Axios.post("/search", { searchTerm: state.searchTerm }, { cancelToken: ourRequest.token })
+          //console.log(response.data)
+          setState(draft => {
+            draft.results = response.data
+            draft.show = "results"
+          })
+        } catch (e) {
+          console.log("There was a problem or the request was cancelled")
+        }
+      }
+
+      fetchResults()
+      return () => ourRequest.cancel
+    }
+  }, [state.requestCount])
+
+  function handleInput(e) {
+    const value = e.target.value
+
+    setState(draft => {
+      draft.searchTerm = value
+    })
+  }
+
   return (
     <div className="search-overlay">
       <div className="search-overlay-top shadow-sm">
@@ -23,7 +78,7 @@ function Search() {
           <label htmlFor="live-search-field" className="search-overlay-icon">
             <i className="fas fa-search"></i>
           </label>
-          <input autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
+          <input onChange={handleInput} autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
           <span onClick={() => appDispatch({ type: "closeSearch" })} className="close-live-search">
             <i className="fas fa-times-circle"></i>
           </span>
@@ -32,7 +87,8 @@ function Search() {
 
       <div className="search-overlay-bottom">
         <div className="container container--narrow py-3">
-          <div className="live-search-results live-search-results--visible">
+          <div className={"circle-loader" + (state.show == "loading" ? " circle-loader--visible" : "")}></div>
+          <div className={"live-search-results" + (state.show == "results" ? " live-search-results--visible" : "")}>
             <div className="list-group shadow-sm">
               <div className="list-group-item active">
                 <strong>Search Results</strong> (3 items found)
