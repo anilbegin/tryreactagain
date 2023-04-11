c9f50f086e79b17004432348528e4836bcf5dd46
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-04-11 12:19:52 +0530

    Chap 64 step-1 useImmer instead of useState, add follow button visible conditions

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useContext, useState } from "react"
+import React, { useEffect, useContext } from "react"
+import { useImmer } from "use-immer"
 import { useParams } from "react-router-dom"
 import Axios from "axios"
 import StateContext from "../StateContext"
@@ -9,11 +10,16 @@ import ProfilePosts from "./ProfilePosts"
 function Profile() {
   const { username } = useParams()
   const appState = useContext(StateContext)
-  const [profileData, setProfileData] = useState({
-    profileUsername: "...",
-    profileAvatar: "https://gravatar.com/avatar/placeholder?s=128",
-    isFollowing: false,
-    counts: { postCount: 0, followerCount: 0, followingCount: 0 }
+  const [state, setState] = useImmer({
+    followActionLoading: false,
+    startFollowingRequestCount: 0,
+    stopFollowingRequestCount: 0,
+    profileData: {
+      profileUsername: "...",
+      profileAvatar: "https://gravatar.com/avatar/placeholder?s=128",
+      isFollowing: false,
+      counts: { postCount: 0, followerCount: 0, followingCount: 0 }
+    }
   })
 
   useEffect(() => {
@@ -21,7 +27,9 @@ function Profile() {
     async function profileInfo() {
       try {
         const response = await Axios.post(`/profile/${username}`, { token: appState.user.token }, { cancelToken: ourRequest.token })
-        setProfileData(response.data)
+        setState(draft => {
+          draft.profileData = response.data
+        })
       } catch (e) {
         console.log("There was a problem")
       }
@@ -31,27 +39,28 @@ function Profile() {
     return () => {
       ourRequest.cancel()
     }
-    return () => {}
   }, [])
 
   return (
     <Page title="Your Profile">
       <h2>
-        <img className="avatar-small" src={profileData.profileAvatar} /> {profileData.profileUsername}
-        <button className="btn btn-primary btn-sm ml-2">
-          Follow <i className="fas fa-user-plus"></i>
-        </button>
+        <img className="avatar-small" src={state.profileData.profileAvatar} /> {state.profileData.profileUsername}
+        {appState.loggedIn && appState.user.username != state.profileData.profileUsername && !state.profileData.isFollowing && state.profileData.profileUsername != "..." && (
+          <button className="btn btn-primary btn-sm ml-2">
+            Follow <i className="fas fa-user-plus"></i>
+          </button>
+        )}
       </h2>
 
       <div className="profile-nav nav nav-tabs pt-2 mb-4">
         <a href="#" className="active nav-item nav-link">
-          Posts: {profileData.counts.postCount}
+          Posts: {state.profileData.counts.postCount}
         </a>
         <a href="#" className="nav-item nav-link">
-          Followers: {profileData.counts.followerCount}
+          Followers: {state.profileData.counts.followerCount}
         </a>
         <a href="#" className="nav-item nav-link">
-          Following: {profileData.counts.followingCount}
+          Following: {state.profileData.counts.followingCount}
         </a>
       </div>
 
