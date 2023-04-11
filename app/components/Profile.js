dd4e404b1f49791c65410941c0bfba476f698b4a
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-04-11 13:10:02 +0530

    Chap 63 step-2 onClick startfollwing stopFollowing functions and usEffect for the same, also stop Following button condition

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -22,6 +22,7 @@ function Profile() {
     }
   })
 
+  // get all information about the Profile
   useEffect(() => {
     const ourRequest = Axios.CancelToken.source()
     async function profileInfo() {
@@ -41,13 +42,86 @@ function Profile() {
     }
   }, [])
 
+  // start following a user
+  useEffect(() => {
+    if (state.startFollowingRequestCount > 0) {
+      setState(draft => {
+        draft.followActionLoading = true
+      })
+
+      const ourRequest = Axios.CancelToken.source()
+      async function startFollowProcess() {
+        try {
+          const response = await Axios.post(`/addFollow/${state.profileData.profileUsername}`, { token: appState.user.token }, { cancelToken: ourRequest.token })
+          setState(draft => {
+            draft.profileData.isFollowing = true
+            draft.profileData.counts.followerCount++
+            draft.followActionLoading = false
+          })
+        } catch (e) {
+          console.log("There was a problem")
+        }
+      }
+
+      startFollowProcess()
+      return () => {
+        ourRequest.cancel()
+      }
+    }
+  }, [state.startFollowingRequestCount])
+
+  // stop following a user
+  useEffect(() => {
+    if (state.stopFollowingRequestCount > 0) {
+      setState(draft => {
+        draft.followActionLoading = true
+      })
+
+      const ourRequest = Axios.CancelToken.source()
+      async function stopFollowProcess() {
+        try {
+          const response = await Axios.post(`/removeFollow/${state.profileData.profileUsername}`, { token: appState.user.token }, { cancelToken: ourRequest.token })
+          setState(draft => {
+            draft.profileData.isFollowing = false
+            draft.profileData.counts.followerCount--
+            draft.followActionLoading = false
+          })
+        } catch (e) {
+          console.log("There was a problem")
+        }
+      }
+
+      stopFollowProcess()
+      return () => {
+        ourRequest.cancel()
+      }
+    }
+  }, [state.stopFollowingRequestCount])
+
+  function startFollowing() {
+    setState(draft => {
+      draft.startFollowingRequestCount++
+    })
+  }
+
+  function stopFollowing() {
+    setState(draft => {
+      draft.stopFollowingRequestCount++
+    })
+  }
+
   return (
     <Page title="Your Profile">
       <h2>
         <img className="avatar-small" src={state.profileData.profileAvatar} /> {state.profileData.profileUsername}
         {appState.loggedIn && appState.user.username != state.profileData.profileUsername && !state.profileData.isFollowing && state.profileData.profileUsername != "..." && (
-          <button className="btn btn-primary btn-sm ml-2">
+          <button onClick={startFollowing} disabled={state.followActionLoading} className="btn btn-primary btn-sm ml-2">
             Follow <i className="fas fa-user-plus"></i>
+          </button>
+        )}
+        {appState.loggedIn && appState.user.username != state.profileData.profileUsername && state.profileData.isFollowing && state.profileData.profileUsername != "..." && (
+          <button onClick={stopFollowing} disabled={state.followActionLoading} className="btn btn-danger btn-sm ml-2">
+            Stop Following <i className="fas fa-user-times"></i>
           </button>
         )}
       </h2>
