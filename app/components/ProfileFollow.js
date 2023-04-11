03e9103838e3b47a7ee3ad5f4622e077953ff13e
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-04-11 17:55:20 +0530

    Chapter 64 extra credits making a common Profilefollow file instead of 2 separate files for follower and following

---
+++ app/components/ProfileFollow.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react"
+import { useParams, Link } from "react-router-dom"
+import Axios from "axios"
+import LoadingDotsIcon from "./LoadingDotsIcon"
+
+function ProfileFollow(props) {
+  const { username } = useParams()
+  const [isLoading, setIsLoading] = useState(true)
+  const [posts, setPosts] = useState()
+
+  useEffect(() => {
+    const ourRequest = Axios.CancelToken.source()
+    async function fetchPosts() {
+      try {
+        const response = await Axios.get(`/profile/${username}/${props.action}`, { cancelToken: ourRequest.token })
+        setPosts(response.data)
+        setIsLoading(false)
+      } catch (e) {
+        console.log("There was a problem")
+      }
+    }
+    fetchPosts()
+    return () => {
+      ourRequest.cancel()
+    }
+  }, [props.action])
+
+  if (isLoading) return <LoadingDotsIcon />
+
+  return (
+    <div className="list-group">
+      {posts.length == 0 && <p className="list-group-item text-muted lead">{props.action == "followers" ? "No Followers yet" : "Not Following anyone yet."}</p>}
+      {posts.map((follower, index) => {
+        return (
+          <Link key={index} to={`/profile/${follower.username}`} className="list-group-item list-group-item-action">
+            <img className="avatar-tiny" src={follower.avatar} /> {follower.username}
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
+
+export default ProfileFollow
